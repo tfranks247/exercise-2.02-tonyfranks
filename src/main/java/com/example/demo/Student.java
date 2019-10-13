4de56683006f11ef8497cc106545ed28877c67b0
Tony <admin@Tonys-MacBook-Pro-5.local>
2019-10-13 13:02:28 -0400

    firt push

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+    private int height;
+    private int number;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
